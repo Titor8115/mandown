a5df87698bb26b59e43d48ffeb0ece00bc546a33
Titor8115 <henryl8115@gmail.com>
2023-04-17 20:52:49 -0400

    update gitignore

--- src/config.c
+++ src/config.c
@@ -17,12 +17,12 @@
  * along with Mandown.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "config.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#include "config.h"
 
 static struct mdn_cfg rc_rule =
     {
